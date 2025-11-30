12597e6995d94a05580062e2bbdc09e1c6cda6a4
RegevO <regevosher@gmail.com>
2025-11-30 17:23:19 +0200

    Regev adding Order DB Table

---
+++ team3/order_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [Order](
+   [order_id]TEXT,
+   [table_number]INTEGER,
+   [status]TEXT,
+   [start_time]TEXT,
+   [end_time]TEXT,
+   [club_membership]TEXT,
+   [total_price]TEXT,
+   [tip]TEXT,
+   [waiter_id]INTEGER
+
+)
